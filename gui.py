393770d83c02542f0843ca904e07671cd3c36ada
Blair Cosgrove <57191365+Cosy8@users.noreply.github.com>
2020-12-13 14:11:28 -0500

    Updated formatting

--- gui.py
+++ gui.py
@@ -6,11 +6,11 @@ class gui:
     def __init__(self):
         root = tk.Tk()
         root.title('Wifi Passwords')
-        root.geometry('400x360')
+        root.geometry('350x280')
         root.protocol("WM_PROTOCOL", sys.exit)
 
         #*  Scrolled text attributes
-        self.text = scrolledtext.ScrolledText(root, height=20)
+        self.text = scrolledtext.ScrolledText(root,height=15)
         self.text.configure(font=("Courier", 10))
         self.text.edit_modified(False)   #!  Auto follow the inserted text
         def showEnd(event): 
@@ -28,4 +28,5 @@ class gui:
         root.mainloop()
 
     def clicked(self):
+        self.text.delete(1.0, END)
         wifi(self.text)
--- wifi_passwords.py
+++ wifi_passwords.py
@@ -1,18 +1,28 @@
 import subprocess
 from tkinter import END
 
+from prettytable import PrettyTable
+
+
 class wifi:
     def __init__(self, text):
+        x = PrettyTable()
+
         data = subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8').split('\n')
         profiles =  [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 
-        text.insert(END, "{:<30}|  {:<}\n\n".format("Wi-fi Name", "Password"))
+        x.field_names = ["Wi-fi Name".center(17), "Password".center(17)]
+        text.insert(END, x)
         for i in profiles:
             results = subprocess.check_output(['netsh','wlan','show','profiles', i, 'key=clear']).decode('utf-8').split('\n')
             results = [r.split(":")[1][1:-1] for r in results if "Key Content" in r]
 
             try:
-                text.insert(END, "{:<30}|  {:<}\n".format(i, results[0]))
+                text.delete(1.0, END)
+                x.add_row([i, results[0]])
+                text.insert(END, x)
             except IndexError:
-                text.insert(END, "{:<30}|  {:<}\n".format(i, ""))
+                text.delete(1.0, END)
+                x.add_row([i, ""])
+                text.insert(END, x)
             text.update()
